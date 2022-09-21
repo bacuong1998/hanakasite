35e973ced922afe3b0ad7206b544e1d55bc68c99
theanhvu1501 <theanhvu1501@gmail.com>
2022-09-21 22:48:44 +0700

    update menu

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ function openMenu(elem) {
 function openSubmenu(elem) {
   var subMenu = document.getElementById("submenu");
   if (subMenu.clientHeight === 0) {
-    subMenu.style.overflow = "auto";
     subMenu.style.height = "auto";
   } else {
     subMenu.style.height = 0;
